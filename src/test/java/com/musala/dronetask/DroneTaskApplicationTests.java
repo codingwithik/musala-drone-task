5e7b5fe08dbbcbb4542e8b86b38dc4567b85232c
codingwithik <ik.francis@yahoo.com>
2021-12-28 18:03:51 -0500

    Bootstraped the application

---
+++ src/test/java/com/musala/dronetask/DroneTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musala.dronetask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
